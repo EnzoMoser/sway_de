359def10139267183082d603a14198623b275187
Enzo Moser <irlenzom@gmail.com>
2024-09-16 23:03:57 +0100

    Actually made install.sh use absolute paths

--- install.sh
+++ install.sh
@@ -1,23 +1,25 @@
 #!/usr/bin/env bash
 set -eo pipefail # Exit if any command fails. Print command.
 
-# Get the correct location for this file.
+# Get the absolute location for this file.
 SOURCE=${BASH_SOURCE[0]}
 while [ -L "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
   DIR=$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )
   SOURCE=$(readlink "$SOURCE")
   [[ $SOURCE != /* ]] && SOURCE=$DIR/$SOURCE # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
 done
+# Get the absolute directory.
 DIR=$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )
 
+# If the command 'pacman' exists, use it to install packages.
 echo "Checking if command 'pacman' exists..."
-# If the command pacman exists, use it to install packages.
 if command -v pacman &> /dev/null; then
   echo "pacman exists! Installing packages..."
   # Return space separated list of packages.
-  export essential_pkg=$(awk -F '#' 'BEGIN{OFS="#";} { if (!/#/) ;else $NF="";print $0}' package-list.txt | sed -n 's/#$//g;p' | grep -v "^$" | tr '\n' ' ')
-  sudo pacman -S $essential_pkg
+  export essential_pkg=$(awk -F '#' 'BEGIN{OFS="#";} { if (!/#/) ;else $NF="";print $0}' $DIR/package-list.txt | sed -n 's/#$//g;p' | grep -v "^$" | tr '\n' ' ')
+  sudo pacman -S --needed --noconfirm $essential_pkg
 else
+  # Otherwise, ask if script should continue.
   echo "WARNING: Package manager pacman not found. Packages MUST be installed manually."
   read -p ":: Continue to symlink dotfiles? [y/N]: " -n 1 -r
   echo    # (optional) move to a new line
@@ -29,13 +31,13 @@ fi
 
 # Set the target to $XDG_CONFIG_HOME, otheriwse $HOME/.config. 
 if [[ -n "$XDG_CONFIG_HOME" ]]; then
-   sed -i 's/--target.*/--target=\$XDG_CONFIG_HOME/' .config/.stowrc
+   sed -i 's/--target.*/--target=\$XDG_CONFIG_HOME/' $DIR/.config/.stowrc
 else
-   sed -i 's/--target.*/--target=\$HOME\/\.config/' .config/.stowrc
+   sed -i 's/--target.*/--target=\$HOME\/\.config/' $DIR/.config/.stowrc
 fi
 
-echo "Calling 'stow' within directoires to symlink..."
 # Setup symlinks using stow
+echo "Calling 'stow' within directoires to symlink..."
 cd $DIR/.config # Cd into dotfile config
 pwd
 stow .
