65abecb08b83070dd837c2db91f70f1b6c6b2d33
Enzo Moser <irlenzom@gmail.com>
2024-09-16 17:21:30 +0100

    Updated install script to call pacman and install packages.

--- install.sh
+++ install.sh
@@ -1,3 +1,22 @@
+#!/usr/bin/env bash
+set -eo pipefail # Exit if any command fails. Print command.
+
+echo "Checking if command 'pacman' exists..."
+# If the command pacman exists, use it to install packages.
+if command -v pacman &> /dev/null; then
+  echo "pacman exists! Installing packages..."
+  # Return space separated list of packages.
+  export essential_pkg=$(awk -F '#' 'BEGIN{OFS="#";} { if (!/#/) ;else $NF="";print $0}' package-list.txt | sed -n 's/#$//g;p' | grep -v "^$" | tr '\n' ' ')
+  sudo pacman -S $essential_pkg
+else
+  echo "WARNING: Package manager pacman not found. Packages MUST be installed manually."
+  read -p ":: Continue to symlink dotfiles? [y/N]: " -n 1 -r
+  echo    # (optional) move to a new line
+  if [[ "$REPLY" != "y" && "$REPLY" != "Y" ]]; then
+    echo "Exiting without symlinking..."
+    exit 1
+  fi
+fi
 
 # Set the target to $XDG_CONFIG_HOME, otheriwse $HOME/.config. 
 if [[ -n "$XDG_CONFIG_HOME" ]]; then
@@ -6,5 +25,8 @@ else
    sed -i 's/--target.*/--target=\$HOME\/\.config/' .config/.stowrc
 fi
 
-# Install using stow
-stow -n .config # Dry-run only right now
+echo "Calling 'stow' within directoires to symlink..."
+# Setup symlinks using stow
+stow .config
+
+echo "DONE!!!"
