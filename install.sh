3a31903ea205c48563cb733cd1a59c2a35c418da
Enzo Moser <irlenzom@gmail.com>
2024-09-18 03:35:20 +0100

    .

--- install.sh
+++ install.sh
@@ -11,24 +11,6 @@ done
 # Get the absolute directory.
 DIR=$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )
 
-# If the command 'pacman' exists, use it to install packages.
-echo "Checking if command 'pacman' exists..."
-if command -v pacman &> /dev/null; then
-  echo "pacman exists! Installing packages..."
-  # Return space separated list of packages.
-  essential_pkg=$(awk -F '#' 'BEGIN{OFS="#";} { if (!/#/) ;else $NF="";print $0}' $DIR/package-list.txt | sed -n 's/#$//g;p' | grep -v "^$" | tr '\n' ' ')
-  sudo pacman -S --needed --noconfirm $essential_pkg
-else
-  # Otherwise, ask if script should continue.
-  echo "WARNING: Package manager pacman not found. Packages MUST be installed manually."
-  read -p ":: Continue to symlink dotfiles? [y/N]: " -n 1 -r
-  echo    # (optional) move to a new line
-  if [[ "$REPLY" != "y" && "$REPLY" != "Y" ]]; then
-    echo "Exiting without symlinking..."
-    exit 1
-  fi
-fi
-
 # Set the target to $XDG_CONFIG_HOME, otheriwse $HOME/.config. 
 if [[ -n "$XDG_CONFIG_HOME" ]]; then
    sed -i 's/--target.*/--target=\$XDG_CONFIG_HOME/' $DIR/.config/.stowrc
@@ -41,5 +23,17 @@ echo "Calling 'stow' within directoires to symlink..."
 cd $DIR/.config # Cd into dotfile config
 stow .
 cd $DIR # Cd back out
+
+# If the command 'pacman' exists, use it to install packages.
+echo "Checking if command 'pacman' exists..."
+if command -v pacman &> /dev/null; then
+  echo "pacman exists! Installing packages..."
+  # Return space separated list of packages.
+  essential_pkg=$(awk -F '#' 'BEGIN{OFS="#";} { if (!/#/) ;else $NF="";print $0}' $DIR/package-list.txt | sed -n 's/#$//g;p' | grep -v "^$" | tr '\n' ' ')
+  sudo pacman -S --needed --noconfirm $essential_pkg
+else
+  # Otherwise, ask if script should continue.
+  echo "WARNING: Package manager pacman not found. Packages MUST be installed manually."
+fi
 
 echo "DONE!!!"
